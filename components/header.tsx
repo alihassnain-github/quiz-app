9ef9e04d015271a1349d096fce31509087da3104
alihassnain-github <alimurtaza32721@gmail.com>
2024-09-24 12:40:57 +0500

    logout user function implement.

--- components/header.tsx
+++ components/header.tsx
@@ -2,42 +2,81 @@
 
 import Link from "next/link";
 import { logout } from "@/firebase/firebaseauth";
-import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Navbar, NavbarBrand, NavbarContent, useDisclosure } from "@nextui-org/react";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, User } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Header() {
 
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [isLoading, setIsLoading] = useState(false);
+
     function logoutUser() {
-        logout();
+
+        setIsLoading(true);
+        logout()
+            .then(() => {
+                setIsLoading(false);
+            })
+            .catch(() => {
+                setIsLoading(false);
+            });
     }
 
     return (
-        <Navbar>
-            <NavbarContent>
-                <NavbarBrand>
-                    <p className="font-semibold">Quiz Portal</p>
-                </NavbarBrand>
-                <Dropdown>
-                    <DropdownTrigger>
-                        <Button className="bg-white"
-                        >
+        <>
+            <Navbar>
+                <NavbarContent>
+                    <NavbarBrand>
+                        <p className="font-semibold">Quiz Portal</p>
+                    </NavbarBrand>
+                    <Dropdown placement="bottom-start">
+                        <DropdownTrigger>
                             <User
-                                name="Jane Doe"
+                                as="button"
                                 avatarProps={{
-                                    src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+                                    isBordered: true,
+                                    src: "",
                                 }}
+                                className="transition-transform"
+                                name="Tony Reichert"
                             />
-                        </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label="Static Actions">
-                        <DropdownItem key="settings"><Link href="appsettings">Settings</Link></DropdownItem>
-                        <DropdownItem onClick={logoutUser} key="delete" className="text-danger" color="danger">
-                            Logout
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-            </NavbarContent>
-        </Navbar >
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="User Actions" variant="flat">
+                            <DropdownItem key="settings">
+                                <Link href="appsettings"> My Settings</Link>
+                            </DropdownItem>
+                            <DropdownItem onClick={onOpen} key="logout" color="danger">
+                                Log Out
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </NavbarContent>
+            </Navbar >
+
+
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">Log out?</ModalHeader>
+                            <ModalBody>
+                                <p>Are you sure you want to log out?</p>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button variant="light" onPress={onClose}>
+                                    Cancel
+                                </Button>
+                                <Button isLoading={isLoading} onClick={logoutUser} color="danger" variant="light">
+                                    Log out
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+
     );
 
 }
